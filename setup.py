0d90ef1fa860a51d680099ec62ed9105387a499d
hrosspet <hrosspet@gmail.com>
2019-02-02 13:45:57 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='precommit_analysis',
+    version=1,
+    packages=['precommit_analysis'],
+    description="Experiments for analysis of pre-commit in OpenAI debate",
+    long_description="",
+    url='https://github.com/hrosspet/precommit_analysis.git',
+)
